5ab450f8957f8f8c5a5b64395f8b6ce01558fb25
Hdpjiax <garia350@gmail.com>
2026-06-20 03:09:45 -0600

    refactor: import renderer shell through views facade

--- src/renderer/app.js
+++ src/renderer/app.js
@@ -1,5 +1,5 @@
 import { state, ui, load, render, setState, normalize, save, setRenderFn, setBindFn, native, defaults } from "./state.js";
-import { renderShell } from "./views/shell-view.js";
+import { renderShell } from "./views/index.js";
 import { bind, initKeyboard, startScheduler } from "./actions.js";
 import { clone } from "./helpers.js";
 
