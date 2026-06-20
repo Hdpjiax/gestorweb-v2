7b58ae03c585889a27b8131f9caad97f2f75643b
Hdpjiax <garia350@gmail.com>
2026-06-20 03:12:39 -0600

    refactor: connect info views through modular shell

--- src/renderer/views/shell-view.js
+++ src/renderer/views/shell-view.js
@@ -1,7 +1,22 @@
+import { state } from "../state.js";
 import { renderOriginalView } from "./original-view.js";
+import { renderSidebar } from "./sidebar-view.js";
+import { renderTopbar } from "./topbar-view.js";
+import { renderInfoView } from "./info-view-router.js";
+import { renderLegacyOverlays } from "./overlay-view.js";
+
+const MODULAR_INFO_VIEWS = new Set(["history", "stats", "settings"]);
 
 export function renderShell() {
-  return renderOriginalView();
+  if (!MODULAR_INFO_VIEWS.has(state.view)) return renderOriginalView();
+
+  return renderShellFrame({
+    sidebar: renderSidebar(),
+    topbar: renderTopbar(),
+    main: renderInfoView(state.view),
+    overlays: renderLegacyOverlays(),
+    showInspector: false
+  });
 }
 
 export function renderShellFrame({ sidebar = "", topbar = "", main = "", inspector = "", overlays = "", showInspector = false } = {}) {
