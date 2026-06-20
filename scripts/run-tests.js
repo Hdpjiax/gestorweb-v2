6b903a36976af8767f97780dcd7fb9c228b46815
Hdpjiax <garia350@gmail.com>
2026-06-20 03:15:24 -0600

    test: include monitor view test

--- scripts/run-tests.js
+++ scripts/run-tests.js
@@ -10,7 +10,8 @@ const tests = [
   ["renderer/inspector-tabs", "../tests/renderer/inspector-tabs.test.js"],
   ["renderer/info-views", "../tests/renderer/info-views.test.js"],
   ["renderer/views-facade", "../tests/renderer/views-facade.test.js"],
-  ["renderer/shell-routing", "../tests/renderer/shell-routing.test.js"]
+  ["renderer/shell-routing", "../tests/renderer/shell-routing.test.js"],
+  ["renderer/monitor-view", "../tests/renderer/monitor-view.test.js"]
 ];
 
 async function run() {
