6058a8ce320d463b425fac3e84c7c5dd6cebb0f3
Hdpjiax <garia350@gmail.com>
2026-06-20 02:39:22 -0600

    refactor: route renderer through shell module

--- src/renderer/app.js
+++ src/renderer/app.js
@@ -1,5 +1,5 @@
 import { state, ui, load, render, setState, normalize, save, setRenderFn, setBindFn, native, defaults } from "./state.js";
-import { renderShell } from "./views.js";
+import { renderShell } from "./views/shell-view.js";
 import { bind, initKeyboard, startScheduler } from "./actions.js";
 import { clone } from "./helpers.js";
 
@@ -11,9 +11,6 @@ startScheduler();
 async function bootstrap() {
   try {
     setState(normalize(await load()));
-    if (native?.license?.hwid && !localStorage.getItem("gestor-web-rebuild:hwid")) {
-      localStorage.setItem("gestor-web-rebuild:hwid", await native.license.hwid());
-    }
   } catch (error) {
     console.error(error);
     setState(normalize(clone(defaults)));
