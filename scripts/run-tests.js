610abc31f6b3c2fdc2a2c91ebbee6f12f0c9134d
Hdpjiax <garia350@gmail.com>
2026-06-20 03:10:12 -0600

    test: include renderer views facade test

--- scripts/run-tests.js
+++ scripts/run-tests.js
@@ -8,7 +8,8 @@ const tests = [
   ["renderer/ux-polish", "../tests/renderer/ux-polish.test.js"],
   ["renderer/inspector-score", "../tests/renderer/inspector-score.test.js"],
   ["renderer/inspector-tabs", "../tests/renderer/inspector-tabs.test.js"],
-  ["renderer/info-views", "../tests/renderer/info-views.test.js"]
+  ["renderer/info-views", "../tests/renderer/info-views.test.js"],
+  ["renderer/views-facade", "../tests/renderer/views-facade.test.js"]
 ];
 
 async function run() {
