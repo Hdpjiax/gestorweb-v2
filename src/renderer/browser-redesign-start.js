70c0100209a965e872dac00ba7186c83ffc137d3
Hdpjiax <garia350@gmail.com>
2026-06-20 02:05:06 -0600

    security: install renderer URL guard before browser fixes

--- src/renderer/browser-redesign-start.js
+++ src/renderer/browser-redesign-start.js
@@ -1,4 +1,6 @@
+import { installRendererUrlGuard } from "./url-guard.js";
 import { installBrowserNativeFix } from "./browser-native-fix.js";
 import "./browser-soft-fonts.js";
 
+installRendererUrlGuard();
 installBrowserNativeFix();
