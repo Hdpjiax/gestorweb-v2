8c47460d1b9a0ad2aa2c61605e47bbe6208a60d7
Hdpjiax <garia350@gmail.com>
2026-06-20 02:15:11 -0600

    ux: install renderer polish layer

--- src/renderer/browser-redesign-start.js
+++ src/renderer/browser-redesign-start.js
@@ -1,6 +1,8 @@
 import { installRendererUrlGuard } from "./url-guard.js";
+import { installUxPolish } from "./ux-polish.js";
 import { installBrowserNativeFix } from "./browser-native-fix.js";
 import "./browser-soft-fonts.js";
 
 installRendererUrlGuard();
+installUxPolish();
 installBrowserNativeFix();
