d4662720e6aa8c2ccfe3b634b51709f2605ac7a7
Hdpjiax <garia350@gmail.com>
2026-06-20 04:25:32 -0600

    test: include persistent topbar test

--- scripts/run-tests.js
+++ scripts/run-tests.js
@@ -15,7 +15,8 @@ const tests = [
   ["renderer/schedules-view", "../tests/renderer/schedules-view.test.js"],
   ["renderer/browse-view", "../tests/renderer/browse-view.test.js"],
   ["renderer/browser-corrections", "../tests/renderer/browser-corrections.test.js"],
-  ["renderer/browser-shell-size", "../tests/renderer/browser-shell-size.test.js"]
+  ["renderer/browser-shell-size", "../tests/renderer/browser-shell-size.test.js"],
+  ["renderer/topbar-always", "../tests/renderer/topbar-always.test.js"]
 ];
 
 async function run() {
