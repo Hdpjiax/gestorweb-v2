0b6d17144c5dbfa6fb6a7c8747c249a2a2e9bc42
Hdpjiax <garia350@gmail.com>
2026-06-20 03:09:33 -0600

    refactor: add renderer views facade

---
+++ src/renderer/views/index.js
@@ -0,0 +1,7 @@
+export { renderShell, renderShellFrame } from "./shell-view.js";
+export { renderSidebar } from "./sidebar-view.js";
+export { renderTopbar } from "./topbar-view.js";
+export { renderInfoView } from "./info-view-router.js";
+export { renderInspectorView } from "./inspector-view.js";
+export { renderInspectorKnownTab } from "./inspector-tab-router.js";
+export { renderProfileViewWithRow } from "./profile-view.js";
