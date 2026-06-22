2b8cc37d65ba0a155520dd59940c3e6ca9942da0
Hdpjiax <garia350@gmail.com>
2026-06-21 20:35:43 -0600

    Update admin license tests

--- tests/main/license-admin.test.js
+++ tests/main/license-admin.test.js
@@ -1,9 +1,8 @@
 const assert = require("assert/strict");
-const { normalizeServerUrl } = require("../../src/main/license-admin-ipc");
+const { normalizeSupabaseUrl } = require("../../src/main/license-admin-ipc");
 
 module.exports = async function licenseAdminTests() {
-  assert.equal(normalizeServerUrl("https://licenses.example.com/"), "https://licenses.example.com");
-  assert.equal(normalizeServerUrl("http://127.0.0.1:8787"), "http://127.0.0.1:8787");
-  assert.throws(() => normalizeServerUrl("http://licenses.example.com"), /HTTPS/);
-  assert.throws(() => normalizeServerUrl("file:///tmp/license"), /HTTPS/);
+  assert.equal(normalizeSupabaseUrl("https://licenses.example.com/"), "https://licenses.example.com");
+  assert.equal(normalizeSupabaseUrl("https://project-ref.supabase.co"), "https://project-ref.supabase.co");
+  assert.throws(() => normalizeSupabaseUrl("http://licenses.example.com"), /HTTPS/);
 };
